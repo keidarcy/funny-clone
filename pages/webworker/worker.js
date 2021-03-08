339adfd14016111434c1952ae5003e8dc4bf21be
XING YAHAO <keidarcy.1015@gmail.com>
2021-03-08 23:10:01 +0900

    webworker

---
+++ pages/webworker/worker.js
@@ -0,0 +1,15 @@
+addEventListener('message', (d) => {
+  const imageData = d.data;
+  const w = imageData.width;
+  const h = imageData.height;
+  const data = imageData.data;
+
+  for (let x = 0; x < w; x++) {
+    for (let y = 0; y < h; y++) {
+      let index = (x + y * w) * 4;
+      data[index] = data[index] * 1.2;
+    }
+  }
+
+  postMessage(imageData, [imageData.data.buffer]);
+});
